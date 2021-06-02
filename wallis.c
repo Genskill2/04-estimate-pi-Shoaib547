86673e9349edcc1a667dcc66db5b31e2fbd9247c
Shoaib547 <84625981+Shoaib547@users.noreply.github.com>
2021-06-02 13:38:56 +0530

    solution

--- wallis.c
+++ wallis.c
@@ -4,15 +4,6 @@
 #include <math.h>
 
 float wallis_pi(int,float,float);
-float wallis_pi(int iterations,float m,float p){
-  for (int i=0;i<iterations;i++){
-    float t1 = m/(m-1);
-    float t2 = m/(m+1);
-    float p *= t1*t2;
-    m += 2;
-  }
-  return 2*p;
-}
 
 int main(void) {
  int iterations;
@@ -23,5 +14,14 @@ int main(void) {
   pi = wallis_pi(iterations,m,p);
   printf("%f",pi);
   return 0;
+}
+float wallis_pi(int iterations,float m,float p){
+  for (int i=0;i<iterations;i++){
+    float t1 = m/(m-1);
+    float t2 = m/(m+1);
+    float p *= t1*t2;
+    m += 2;
+  }
+  return 2*p;
 }
 
